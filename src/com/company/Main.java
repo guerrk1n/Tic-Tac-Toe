8ec35aedcd8470d6dde6f4524b855efd3822bd8d
YanSidorovich <sidorovich.yan@bk.ru>
2018-12-20 23:09:31 +0300

    yan

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         for(int i=0; i<=5; i++){
-
+            System.out.println("SOLGASEEEEEN");
         System.out.println("Скорей бы Влад и Миша настроили себе GIT и INTELIJ =)");}
     }
 }
