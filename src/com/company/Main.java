78471323d6a528822b69c23c2cb2754b889051da
guerrk1n <aleksej684549@inbox.ru>
2018-12-20 22:58:51 +0300

    checking

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i=0;i<10;i++){
-            System.out.println("kekekeke");
-            System.out.println("kekekeke");
-            System.out.println("kekekeke");
+            System.out.println("YAN");
+
 
 
 
