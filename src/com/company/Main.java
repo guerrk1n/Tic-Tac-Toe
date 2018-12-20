8081133896bcde13280d1d1c0bb1b2bbf5ac0956
guerrk1n <aleksej684549@inbox.ru>
2018-12-20 22:52:17 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Check");
-        System.out.println("chec");
+        for (int i=0;i<10;i++){
+            System.out.println("kekekeke");
+        }
     }
 }
