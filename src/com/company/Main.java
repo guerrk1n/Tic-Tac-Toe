1b787cca8711d6639d0d7551aba5771fe92d7ea9
guerrk1n <aleksej684549@inbox.ru>
2018-12-20 23:05:52 +0300

    checking

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i=0;i<10;i++){
-            System.out.println("YAN");
 
+        for(int i=0; i<=5; i++){
 
-
-
-
-
-        }
+        System.out.println("Скорей бы Влад и Миша настроили себе GIT и INTELIJ =)");}
     }
 }
