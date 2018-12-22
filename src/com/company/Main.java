861a350c6f077b2f2c1dda0120918a092719ba8b
michaelkotor <milan3020@gmail.com>
2018-12-22 16:41:07 +0300

    add boolean isGameOver();

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 5; i++) {
+        int mas[][];
+        mas = new int[3][3];
 
-            System.out.println("Michael was here");
+        mas[0][2] = -1;
+
+        for (int i = 0; i <= 2; i++) {
+            for (int k = 0; k <= 2; k++) {
+                System.out.print(mas[i][k]);
+            }
+            System.out.println("");
         }
     }
 }
--- src/com/company/Position.java
+++ src/com/company/Position.java
@@ -1,5 +1,95 @@
 package com.company;
 
-public class Position {
+public abstract class Position {
+    Object[][] table = new Object[3][3];
 
+    //X is a player, so let's make X will be a char X;
+    //We will change '0' to Y. I think, that way we will be easier to se the difference.
+
+    Position(Object[][] table) {
+        this.table = table;
+    }
+
+    Position() {
+        for (int i = 0; i <= 2; i++) {
+            for (int k = 0; k <= 2; i++) {
+                table[i][k] = 0;
+            }
+        }
+    }
+
+    boolean isGameOver(){
+        if (table[0][0].equals('X'))
+            if (table[0][1].equals('X'))
+                if (table[0][2].equals('X'))
+                    return true;
+        if (table[0][0].equals('Y'))
+            if (table[0][1].equals('Y'))
+                if (table[0][2].equals('Y'))
+                    return true;
+        if (table[1][0].equals('X'))
+            if (table[1][1].equals('X'))
+                if (table[1][2].equals('X'))
+                    return true;
+        if (table[1][0].equals('Y'))
+            if (table[1][1].equals('Y'))
+                if (table[1][2].equals('Y'))
+                    return true;
+        if (table[2][0].equals('X'))
+            if (table[2][1].equals('X'))
+                if (table[2][2].equals('X'))
+                    return true;
+        if (table[2][0].equals('Y'))
+            if (table[2][1].equals('Y'))
+                if (table[2][2].equals('Y'))
+                    return true;
+
+        if (table[0][0].equals('X'))
+            if (table[1][0].equals('X'))
+                if (table[2][0].equals('X'))
+                    return true;
+        if (table[0][0].equals('Y'))
+            if (table[1][0].equals('Y'))
+                if (table[2][0].equals('Y'))
+                    return true;
+        if (table[0][1].equals('X'))
+            if (table[1][1].equals('X'))
+                if (table[2][1].equals('X'))
+                    return true;
+        if (table[0][1].equals('Y'))
+            if (table[1][1].equals('Y'))
+                if (table[2][1].equals('Y'))
+                    return true;
+        if (table[0][1].equals('X'))
+            if (table[1][1].equals('X'))
+                if (table[2][1].equals('X'))
+                    return true;
+        if (table[0][2].equals('Y'))
+            if (table[1][2].equals('Y'))
+                if (table[2][2].equals('Y'))
+                    return true;
+
+        if (table[0][0].equals('X'))
+            if (table[1][1].equals('X'))
+                if (table[2][2].equals('X'))
+                    return true;
+        if (table[0][0].equals('Y'))
+            if (table[1][1].equals('Y'))
+                if (table[2][2].equals('Y'))
+                    return true;
+        if (table[0][2].equals('X'))
+            if (table[1][1].equals('X'))
+                if (table[2][0].equals('X'))
+                    return true;
+        if (table[0][2].equals('Y'))
+            if (table[1][1].equals('Y'))
+                if (table[2][0].equals('Y'))
+                    return true;
+        return false;
+    };
+    //boolean isGoodEnd(){};
+
+    //Vertex MoveBack(){};
+    //Vertex MoveForward(){};
+    //int getMoves(){};
 }
