bb5431ff0d0ba8304595771883a901b6b104ebe0
michaelkotor <milan3020@gmail.com>
2018-12-22 15:04:48 +0300

    My first commit

---
+++ src/com/company/Graph.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Graph {
+    final int MAX_SIZE = 20;
+    private int adjMat[][];
+    Position rigntNow;
+
+    Graph() {
+        adjMat = new int[MAX_SIZE][MAX_SIZE];
+    }
+
+    void Move(int[] a) {};
+
+    Graph createNewGraph(int a) {
+        return new Graph();
+    };
+
+
+}
---
+++ src/com/company/Queue.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Queue<T extends Integer> {
+    private final int SIZE = 20;
+    private Object[] queArray;
+    private int front;
+    private int rear;
+
+    Queue() {
+        queArray = new Object[SIZE];
+        front = 0;
+        rear = -1;
+    }
+
+    void insert(T t) {
+        if (rear == SIZE - 1) {
+            rear = -1;
+        }
+        queArray[++rear] = t;
+    }
+
+    T remove() {
+        T temp = (T)queArray[front++];
+        if (front == SIZE) {
+            front = 0;
+        }
+        return (T)temp;
+    }
+
+    public boolean isEmpty() {
+        return ( rear+1==front || (front+SIZE - 1 == rear) );
+    }
+
+
+}
